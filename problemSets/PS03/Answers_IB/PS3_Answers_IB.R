8c5f683c609df269e16b79e8e02d0803eff9c063
Ivy-B1rch <birchali@tcd.ie>
2026-03-25 01:54:43 +0000

    PS03 Answers
    The Answers

--- problemSets/PS03/Answers_IB/PS3_Answers_IB.R
+++ problemSets/PS03/Answers_IB/PS3_Answers_IB.R
@@ -27,7 +27,7 @@ pkgTest <- function(pkg){
 # ex: stringr
 # lapply(c("stringr"),  pkgTest)
 
-lapply(c("nnet", "MASS"),  pkgTest)
+lapply(c("nnet", "MASS","tidyverse"),  pkgTest)
 
 # set wd for current folder
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -38,6 +38,21 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # load data
 gdp_data <- read.csv("https://raw.githubusercontent.com/ASDS-TCD/StatsII_2026/main/datasets/gdpChange.csv", stringsAsFactors = F)
+gdp_data$GDPWdiffcat <- cut(gdp_data$GDPWdiff,
+                        breaks = c(-Inf, 0, Inf),
+                        labels = c("Decrease", "Increase"))
+
+gdp_data$GDPWdiffcat <- as.character(gdp_data$GDPWdiffcat)
+gdp_data$GDPWdiffcat[gdp_data$GDPWdiff == 0] <- "No change"
+gdp_data$GDPWdiffcat <- factor(gdp_data$GDPWdiffcat)
+gdp_data$GDPWdiffcat <- relevel(gdp_data$GDPWdiffcat, ref = "No change")
+
+model <- multinom(GDPWdiffcat ~ REG + OIL, data = gdp_data)
+summary(model)
+
+model.ord <-polr(GDPWdiffcat ~ REG + OIL, data = gdp_data, Hess =TRUE)
+model.ord
+
 
 
 
@@ -47,3 +62,10 @@ gdp_data <- read.csv("https://raw.githubusercontent.com/ASDS-TCD/StatsII_2026/ma
 
 # load data
 mexico_elections <- read.csv("https://raw.githubusercontent.com/ASDS-TCD/StatsII_2026/main/datasets/MexicoMuniData.csv")
+
+#my system has an evil bug involving R studio that wont let me save the model as an object so the code shall be a bit jank
+summary(glm(PAN.visits.06 ~ competitive.district + marginality.06 + PAN.governor.06,
+             family = poisson, data = mexico_elections))
+
+
+exp(-3.81023 + -0.08135 + -2.08014)
--- tutorials/Week 8/tutorial08.R
+++ tutorials/Week 8/tutorial08.R
@@ -37,46 +37,165 @@ lapply(c("MASS", "nnet", "ggplot2"),  pkgTest)
 # set wd for current folder
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# This data set is analyzed by Long (1997).  The response variable has four ordered categories:
-# Strongly Disagree, Disagree, Agree, and Strongly Agree in relation to the statement
-
-# "A working mother can establish just as warm and secure a relationship with her children as a mother who does not work."
-
-# The explanatory variables are:
-# the year of the survey (1977 or 1989),
-# the gender of the respondent,
-# the race of the respondent (white or non-white),
-# the respondent's age, and
-# the prestige of the respondent's occupation (a quantitative variable)
-
 # load data
 workingMoms <- read.table("http://statmath.wu.ac.at/courses/StatsWithR/WorkingMoms.txt", header=T)
+# Outcome: attitude toward working mothers
 
-# Re-label your outcome, gender, race, and year so they are legible
-# i.e. "SD", "D", "A", "SA" to "Strongly Disagree", "Disagree", "Agree", "Strongly Agree" 
-# 0,1 to "Non-white", "White"
-# and "Year1977", "Year1989" to "1977", "1989"
+# check basic cross-tabs
+summary(workingMoms)
+ftable(xtabs(~ gender + year + attitude, data = workingMoms))
 
-# Plot prestige (y-axis) by your outcome (x-axis) by gender ~ year
+# do some wrangling
+workingMoms$attitude <- factor(workingMoms$attitude, 
+                               levels = c("SD", "D", "A", "SA"),
+                               labels = c("Strongly Disagree",
+                                          "Disagree",
+                                          "Agree",
+                                          "Strongly Agree"))
+workingMoms$gender <- as.factor(workingMoms$gender)
+workingMoms$race <- factor(workingMoms$race,
+                           levels = c(0,1),
+                           labels = c("Non-white", "White"))
+workingMoms$year <- factor(workingMoms$year,
+                           levels = c("Year1977", "Year1989"),
+                           labels = c("1977", "1989"))
+
+ftable(xtabs(~ gender + year + attitude, data = workingMoms))
+prop.table(table(workingMoms$gender, workingMoms$attitude), 1)
+prop.table(table(workingMoms$gender[workingMoms$year=="1977"],
+                 workingMoms$attitude[workingMoms$year=="1977"]), 1)
+
+prop.table(table(workingMoms$gender[workingMoms$year=="1989"],
+                 workingMoms$attitude[workingMoms$year=="1989"]), 1)
+
+ggplot(workingMoms, aes(attitude, prestige)) +
+  geom_boxplot() +
+  geom_jitter(alpha = 0.3) +
+  scale_x_discrete(labels=function(x){sub("\\s", "\n", x)}) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  facet_grid(gender ~ year)
 
 # a) Perform an ordered (proportional odds) logistic regression
 
+ord.log <- polr(attitude ~ ., data = workingMoms, Hess = TRUE)
+summary(ord.log)
+
+pp <- data.frame(fitted(ord.log))
+head(data.frame(attitude = workingMoms$attitude,
+                SD = pp$Strongly.Disagree,
+                D = pp$Disagree,
+                A = pp$Agree,
+                SA = pp$Strongly.Agree))
+
 # Calculate a p value
+ctable <- coef(summary(ord.log))
+p <- pnorm(abs(ctable[, "t value"]), lower.tail = FALSE) * 2
+(ctable <- cbind(ctable, "p value" = p))
+
 # Calculate confidence intervals
-# Convert to odds ratio
+(ci <- confint(ord.log))
+
+# convert to odds ratio
+exp(cbind(OR = coef(ord.log), ci))
 
 # How do we interpret these coefficients?
+## A one-unit increase in X changes the log-odds of being in a higher attitude category by𝛽.
+## The same β applies to every threshold.
+
+## Age: Holding other variables constant, on average, for each additional year of age, the odds of being in a more supportive category decrease by about 2.1%.
+##So, Older people are less supportive of working mothers.
+
+## Education: Each additional year of education increases the odds of being in a more supportive attitude category by about 6.9%.
+##More educated people are more supportive.
+
+## prestige: Higher occupational prestige slightly increases the odds of being more supportive.
+##However, not statistically significant at the 5% level
+
+## Gender: Men have 52% lower odds of being in a more supportive category compared with women.
+##Men are less supportive of working mothers.
+
+## year1989: People in 1989 had about 69% higher odds of being in a more supportive category compared with 1977.
+##Support for working mothers increased over time.
+
+## racewhite: White respondents have 32% lower odds of being in a more supportive category compared with non-white respondents.
+
 
 # b) fit a multinomial logit model
-# with Strongly Disagree as reference level for the outcome
+# set a reference level for the outcome
+workingMoms$attitude <- relevel(workingMoms$attitude, ref = "Strongly Disagree")
+##Compare every category to Strongly Disagree.
 
 # run model
+mult.log <- multinom(attitude ~ ., data = workingMoms)
+summary(mult.log)
+##So each predictor has different coefficients for each category.
+##This is the main difference from ordered logit.
+
+## Interpretation:
+##Agree vs Strongly Disagree (age = -0.025): A one-year increase in age decreases the log-odds of agreeing (vs strongly disagreeing) by 0.025.
+
+exp(coef(mult.log))
 # get p values
+z <- summary(mult.log)$coefficients/summary(mult.log)$standard.errors
+(p <- (1 - pnorm(abs(z), 0, 1)) * 2)
+
 # how do we interpret these coefficients?
 
-# calculate predicted probabilities to help our interpretation
+## Interpretation:
+##Agree vs Strongly Disagree (age = 0.975): Each additional year of age reduces the odds of Agree vs Strongly Disagree by about 2.5%
 
-# c) Consider gender as an interaction with your other predictors
-# i.e. consider that possibility that gender interacts with the other explanatory variables in influencing the response variable
+##More education increases the odds of being in any more supportive category relative to strongly disagreeing.
+##Each additional year of education increases the odds of strongly agreeing vs strongly disagreeing by about 15%.
 
-# What do you find?
+##Compared with women, men are slightly more likely to say Disagree than Strongly Disagree
+##but much less likely to Agree
+##and far less likely to Strongly Agree
+##Men have about 71% lower odds of strongly agreeing (vs strongly disagreeing) compared with women.
+
+##In 1989, the odds of strongly agreeing (vs strongly disagreeing) were about 3.2 times higher.
+
+##White respondents are less likely to express supportive attitudes relative to strongly disagreeing.
+
+
+# we can use predicted probabilities to help our interpretation
+pp <- data.frame(fitted(mult.log))
+head(data.frame(attitude = workingMoms$attitude,
+                SD = pp$Strongly.Disagree,
+                D = pp$Disagree,
+                A = pp$Agree,
+                SA = pp$Strongly.Agree))
+
+##Multinomial logit coefficients are log-odds, which are difficult to interpret.
+##Predicted probabilities translate the model into something intuitive:
+##“Given these characteristics, what is the probability of each attitude?”
+##For Example: Instead of saying coefficient = 0.45
+##You can say: The model predicts a 45% probability of agreeing.
+
+# c) Consider gender as an interaction
+mult.log.int <- multinom(attitude ~ gender * ., data = workingMoms)
+#Assumption: The effect of every variable on attitude can differ for men and women.
+summary(mult.log.int)
+
+z.int <- summary(mult.log.int)$coefficients/summary(mult.log.int)$standard.errors
+(p.int <- (1 - pnorm(abs(z.int), 0, 1)) * 2)
+
+#Interpretations: Look at the Agree equation: age = -0.011; genderMale:age = -0.0265
+#For women: Effect of age: -0.011
+#Older women are slightly less likely to Agree vs Strongly Disagree.
+#For men: Effect of age: -0.011 + (-0.0265) = -0.0375
+#So: Age reduces the probability of agreeing more strongly for men than for women.
+
+#From Strongly Agree equation: education = 0.2216; genderMale:education = -0.1437
+#For women: Effect of education: 0.2216
+#Education strongly increases the chance of Strongly Agree vs Strongly Disagree.
+#For men: Effect: 0.2216 − 0.1437 = 0.0779
+#So: Education increases support much less for men than for women.
+
+pp.int <- data.frame(fitted(mult.log.int))
+head(data.frame(attitude = workingMoms$attitude,
+                SD = pp.int$Strongly.Disagree,
+                D = pp.int$Disagree,
+                A = pp.int$Agree,
+                SA = pp.int$Strongly.Agree))
+
+#anova(mult.log, mult.log.int, test = "Chisq")
